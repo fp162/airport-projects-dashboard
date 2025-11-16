d199de7c7e752295260c5ee0c49ae77efa8cdcff
fp162 <72213226+fp162@users.noreply.github.com>
2025-11-16 12:37:30 +0000

    Add files via upload

---
+++ app/build-info.js
@@ -0,0 +1,7 @@
+// This file is auto-generated at build time
+// DO NOT EDIT MANUALLY
+
+export const BUILD_INFO = {
+  timestamp: new Date().toISOString(),
+  date: new Date().toISOString().split('T')[0]
+};
